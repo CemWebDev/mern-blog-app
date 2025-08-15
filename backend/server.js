498c4f1cfab6b9c7a339c877fbf8791de6c970c0
Cem <bcemcankaya@gmail.com>
2025-08-15 19:09:25 +0300

    fix: remove fallbacks for railway deploying

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import './src/config/passport.js';
 
 import app from './src/app.js';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
